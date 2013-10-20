0df7e623dfbdb00ca0bc2e9c9393aeee2a135444
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2013-10-20 21:36:02 +0200

    Now Tunes demo is working - a tune with frequency and a c-major scale.

--- HelloTuneModule/Program.cs
+++ HelloTuneModule/Program.cs
@@ -20,22 +20,18 @@ namespace HelloTuneModule
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
-            /*******************************************************************************************
-            Modules added in the Program.gadgeteer designer view are used by typing 
-            their name followed by a period, e.g.  button.  or  camera.
-            
-            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
-                button.ButtonPressed +=<tab><tab>
-            
-            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
-                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
-                timer.Tick +=<tab><tab>
-                timer.Start();
-            *******************************************************************************************/
-
-
             // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
             Debug.Print("Program Started");
+            tunes.AddNote(new Tunes.MusicNote(new Tunes.Tone(440), 500 ));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.C4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.D4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.E4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.F4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.G4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.A4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.B4, 500));
+            tunes.AddNote(new Tunes.MusicNote(Tunes.Tone.C5, 500));
+            tunes.Play();
         }
     }
 }
